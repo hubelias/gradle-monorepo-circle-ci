c9653551d556ad2c16a59fa425254a2e8d4c3672
Hubert Elias <hubert.elias@pragmatists.pl>
2022-08-01 20:53:23 +0200

    Add some tests

---
+++ src/client/src/test/java/dev/hubelias/myproject/client/PeopleClientTest.java
@@ -0,0 +1,17 @@
+package dev.hubelias.myproject.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PeopleClientTest {
+    @Test
+    void shouldPass() {
+        assertThat(true).isTrue();
+    }
+
+    @Test
+    void shouldFail() {
+        assertThat(false).isTrue();
+    }
+}
---
+++ src/server/src/test/java/dev/hubelias/myproject/acceptance/PeopleAcceptanceTest.java
@@ -0,0 +1,12 @@
+package dev.hubelias.myproject.acceptance;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PeopleAcceptanceTest {
+    @Test
+    void shouldPass() {
+        assertThat(true).isTrue();
+    }
+}
