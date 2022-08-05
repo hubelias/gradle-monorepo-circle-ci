a1b262fa367c3ef61c2ee91b0c5f6c1bc5b7be9f
Hubert Elias <hubert.elias@pragmatists.pl>
2022-08-05 12:41:56 +0200

    Download gradle

---
+++ src/server-api/src/test/java/dev/hubelias/myproject/api/FaliningApiTest.java
@@ -0,0 +1,12 @@
+package dev.hubelias.myproject.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class FaliningApiTest {
+    @Test
+    void name() {
+        fail("Just because");
+    }
+}
